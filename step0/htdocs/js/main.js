50df2d21fbb1d02d5ff9bad6b8f7b75eb4133b05
Tetsutaro Endo <tetsutaro.endo@gaiax.com>
2016-05-23 23:54:19 +0900

    0文字バリデーション、投稿モーダル、その他クリックアクション追加

--- step0/htdocs/js/main.js
+++ step0/htdocs/js/main.js
@@ -1,6 +1,44 @@
-function al() {
-  alert("hogeeeee");
-}
+$(function(){
+  function testAlert() {
+    alert("hogeeeee");
+  }
 
-var element = document.querySelector('.post-btn');
-element.addEventListener("click", al, false);
+  $('.cog').click(function(){
+    alert("hogeoooooo");
+  });
+
+  $('.post-btn').click(function(){
+    $('.post-modal').fadeIn(200);
+    $('.overlay').fadeIn(200);
+  });
+
+  $('.command-box .edit').click(function(){
+    alert("hogeoooooo");
+  });
+
+  $('.command-box .delete').click(function(){
+    alert("hogeoooooo");
+  });
+
+  $('.post-box textarea').bind('keydown keyup keypress change', function(){
+    if( $(this).val().length >= 1 ){
+      $(this).next().removeClass('is-disabled');
+    } else {
+      $(this).next().addClass('is-disabled');
+    }
+  });
+
+  $('.overlay').click(function(){
+    $(this).fadeOut(200);
+    $('.post-modal').fadeOut(200);
+    $('.post-box textarea').val('');
+    $('.post-box button').addClass('is-disabled');
+  });
+
+  $('.close').click(function(){
+    $('.overlay').fadeOut(200);
+    $('.post-modal').fadeOut(200);
+    $('.post-box textarea').val('');
+    $('.post-box button').addClass('is-disabled');
+  });
+});
